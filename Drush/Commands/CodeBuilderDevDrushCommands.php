7b6e6b6d6abe95a6b8090dc54d9aa18af6aa1d7e
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-02-15 11:22:08 +0000

    Added devel command for a single collect job.

--- Drush/Commands/CodeBuilderDevDrushCommands.php
+++ Drush/Commands/CodeBuilderDevDrushCommands.php
@@ -6,6 +6,7 @@ use Consolidation\AnnotatedCommand\AnnotationData;
 use Consolidation\AnnotatedCommand\CommandData;
 use Consolidation\AnnotatedCommand\CommandError;
 use Drush\Commands\DrushCommands;
+use Drush\Boot\DrupalBootLevels;
 use Drush\Drush;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -49,6 +50,39 @@ class CodeBuilderDevDrushCommands extends DrushCommands {
     $hooks_directory = \DrupalCodeBuilder\Factory::getEnvironment()->getHooksDirectory();
 
     $output->writeln("Data on hooks, services, and plugin types has been copied to {$hooks_directory} and processed.");
+
+    return TRUE;
+  }
+
+  /**
+   * Outputs the data for a single collect job.
+   *
+   * TODO: Make this a bit nicer - needs a numeric argument!
+   */
+  #[\Drush\Attributes\Command(name: 'cb-update-devel', aliases: ['cbud'])]
+  #[\Drush\Attributes\Argument(name: 'job', description: 'Numeric key of the collect job to process in the job list array.')]
+  #[\Drush\Attributes\Help(hidden: true)]
+  #[\Drush\Attributes\Bootstrap(level: DrupalBootLevels::FULL)]
+  public function commandTestCollect(OutputInterface $output, int $job) {
+    $drupal_root = Drush::bootstrapManager()->getRoot();
+    $drupal_version = Drush::bootstrap()->getVersion($drupal_root);
+
+    \DrupalCodeBuilder\Factory::setEnvironmentLocalClass('Drush')
+      ->setCoreVersionNumber($drupal_version);
+
+    $task_handler_collect = \DrupalCodeBuilder\Factory::getTask('Testing\CollectTesting');
+
+    $job_list = $task_handler_collect->getJobList();
+
+    if (!isset($job_list[$job])) {
+      throw new \InvalidArgumentException("Job $job not found.");
+    }
+
+    // Get the helper from the DCB container.
+    $collector_helper = \DrupalCodeBuilder\Factory::getContainer()->get($job_list[$job]['collector']);
+    $job_data = $collector_helper->collect([$job_list[$job]]);
+
+    dump($job_data);
 
     return TRUE;
   }
